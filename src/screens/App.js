7a25ed2980d72ba25de997027bba9ef1b2679dbb
Kamil Rusin <lotrusek@gmail.com>
2020-03-16 20:00:16 +0100

    Initial commit

---
+++ src/screens/App.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text } from 'react-native';
+
+const App: () => React$Node = () => {
+    return (
+        <>
+            <Text>Dziala</Text>
+        </>
+    );
+};
+
+export default App;
