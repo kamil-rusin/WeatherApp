803575d3c177edfa64b5769fd2bb9b403106c0e0
Kamil Rusin <lotrusek@gmail.com>
2020-03-26 17:21:47 +0100

    feat: add SafeAreaView

--- src/screens/App.js
+++ src/screens/App.js
@@ -4,13 +4,16 @@
  */
 
 import React from 'react';
-
+import { SafeAreaView } from 'react-native';
 import WeatherContainer from './WeatherContainer';
+import { safeAreaViewStyle } from '../styles/SafeAreaViewStyle';
 
 const App: () => React$Node = () => {
     return (
         <>
-            <WeatherContainer />
+            <SafeAreaView style={safeAreaViewStyle.container}>
+                <WeatherContainer />
+            </SafeAreaView>
         </>
     );
 };
